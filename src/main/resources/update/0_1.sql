c88f5cdfa4e91c128e03e7477d979ef041d2abd9
razon <shizonera@gmail.com>
2015-11-03 08:16:55 +0900

    Modify migration SQL

--- src/main/resources/update/0_1.sql
+++ src/main/resources/update/0_1.sql
@@ -1,2 +1,9 @@
--- Add activity trigger
---create trigger trigger_activity after insert on activity for each row call "gitbucket.backchanelling.trigger.ActivityTrigger";
+-- Add back channeling
+CREATE TABLE BACK_CHANNELING(
+  USER_NAME          VARCHAR(100) NOT NULL,
+  REPOSITORY_NAME    VARCHAR(100) NOT NULL,
+  URL                VARCHAR(200) NOT NULL,
+  AUTHORIZATION_CODE VARCHAR(16)  NOT NULL,
+  THREAD_ID          BIGINT        NOT NULL
+);
+ALTER TABLE BACK_CHANNELING ADD PRIMARY KEY (USER_NAME, REPOSITORY_NAME);
